be76d24b700edd5233f95e2a6f3c5ad9bb847aba
Elena Manea <maneaelenasorina@gmail.com>
2023-08-01 12:52:11 +0200

    Initial Express commit

--- app.js
+++ app.js
@@ -17,5 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 app.use(cors()); // add after 'app' is created
+
 module.exports = app;
