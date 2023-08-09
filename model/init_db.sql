0910ca6ce28ca19cd2fea3c817cf30f9a17480e6
Elena Manea <maneaelenasorina@gmail.com>
2023-08-09 12:14:16 +0200

    api.js

--- model/init_db.sql
+++ model/init_db.sql
@@ -4,6 +4,7 @@
 
 SET foreign_key_checks = 0;
 DROP TABLE if exists pizzerias;
+DROP TABLE if exists favourites;
 SET foreign_key_checks = 1;
 
 --
@@ -16,5 +17,12 @@ CREATE TABLE pizzerias (
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL
 );
+
+CREATE TABLE favourites (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pizzeria_id INT NOT NULL,
+    FOREIGN KEY (pizzeria_id) REFERENCES pizzerias(id)
+);
+
 INSERT INTO pizzerias (name, address, latitude, longitude)
         VALUES ('Makekosa', 'C. de Floridablanca, 102, 08015 Barcelona', 41.379709576724245, 2.160408546182025);
--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,76 @@ const db = require("../model/helper");
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({ title: 'api' });
+// router.get('/', function(req, res, next) {
+//   res.send({ title: 'api' });
+// });
+
+//Obtener todas las pizzerias
+// router.get("/pizzerias", async (req, res) => {
+//   try{
+//   const result = await db("SELECT * from pizzerias");
+//   const pizzerias = result.data;
+//   res.send(pizzerias);
+// } catch (error) {
+//   res.send(500);
+// }
+// });
+router.get("/pizzerias", async (req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM pizzerias');
+    const pizzerias = result.data;
+    res.json(pizzerias);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
+// Añade a favoritos 
+router.post('/favourites', async (req, res) => {
+  try {
+    const { pizzeriaId } = req.body;
+
+    if (!pizzeriaId) {
+      return res.status(400).json({ error: 'Missing pizzeriaId' });
+    }
+
+    const result = await db.query('INSERT INTO favourites (pizzeria_id) VALUES (?)', [pizzeriaId]);
+    res.json({ message: 'Pizzeria added to favourites' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Obtener la lista de las pizzerias favoritas
+router.get('/favourites', async (req, res) => {
+  try {
+    const result = await db.query('SELECT p.* FROM pizzerias p JOIN favourites f ON p.id = f.pizzeria_id');
+    const preferredPizzerias = result.data;
+    res.json(preferredPizzerias);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Elimina una pizzeria de los favoritos
+router.delete('/favourites/:id', async (req, res) => {
+  try {
+    const pizzeriaId = req.params.id;
+
+    // Verifico que la pizzeria esté en los favoritos antes de borrarla
+    const checkResult = await db.query('SELECT id FROM favourites WHERE pizzeria_id = ?', [pizzeriaId]);
+    if (checkResult.data.length === 0) {
+      return res.status(404).json({ error: 'Pizzeria not found in favorites.' });
+    }
+
+    const result = await db.query('DELETE FROM favourites WHERE pizzeria_id = ?', [pizzeriaId]);
+    res.json({ message: 'Pizzeria removed from favorites.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 module.exports = router;
